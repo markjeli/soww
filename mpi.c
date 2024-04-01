66124d0db060d3223d057337027f65b2620802ee
markjeli <markandjel@gmail.com>
2024-04-01 09:32:06 +0200

    Formatted document

--- mpi.c
+++ mpi.c
@@ -2,164 +2,174 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <math.h> 
+#include <math.h>
 #include <mpi.h>
 #include "numgen.c"
 
-#define RANGESIZE 5
+#define RANGESIZE 1
 #define DATA 0
 #define RESULT 1
 #define FINISH 2
 
 int isPrime(int n)
 {
-    // Corner case
-    if (n <= 1)
-        return 0;
- 
-    // Check from 2 to n-1
-    for (int i = 2; i <= sqrt(n); i++)
-        if (n % i == 0)
-            return 0;
- 
-    return 1;
+  // Corner case
+  if (n <= 1)
+    return 0;
+
+  // Check from 2 to n-1
+  for (int i = 2; i <= sqrt(n); i++)
+    if (n % i == 0)
+      return 0;
+
+  return 1;
 }
 
-unsigned long CheckHowManyPrimes(unsigned long int numbers[], int size) {
+unsigned long CheckHowManyPrimes(unsigned long int numbers[], int size)
+{
   unsigned long count = 0;
-  for (int i=0;i<size;i++) {
-    if (isPrime(numbers[i])) {
-      count++;      
-    }    
+  for (int i = 0; i < size; i++)
+  {
+    if (isPrime(numbers[i]))
+    {
+      count++;
+    }
   }
   return count;
 }
 
-int main(int argc,char **argv) {
+int main(int argc, char **argv)
+{
 
   Args ins__args;
   parseArgs(&ins__args, &argc, argv);
 
-  //program input argument
-  long inputArgument = ins__args.arg; 
-  
+  // program input argument
+  long inputArgument = ins__args.arg;
+
   struct timeval ins__tstart, ins__tstop;
 
-  int myrank,nproc;
+  int myrank, nproc;
   unsigned long int *numbers;
 
-  int a=0, b=inputArgument;
+  int a = 0, b = inputArgument;
   int range[2];
   int rangeToSend = 0;
-  unsigned long result=0, resulttemp;
-  int sentcount=0;
+  unsigned long result = 0, resulttemp;
   MPI_Status status;
 
-  MPI_Init(&argc,&argv);
+  MPI_Init(&argc, &argv);
 
   // obtain my rank
-  MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   // and the number of processes
-  MPI_Comm_size(MPI_COMM_WORLD,&nproc);
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-  if(!myrank){
-      	gettimeofday(&ins__tstart, NULL);
-	  numbers = (unsigned long int*)malloc(inputArgument * sizeof(unsigned long int));
-  	numgen(inputArgument, numbers);   
+  if (!myrank)
+  {
+    gettimeofday(&ins__tstart, NULL);
+    numbers = (unsigned long int *)malloc(inputArgument * sizeof(unsigned long int));
+    numgen(inputArgument, numbers);
   }
-  
-  if (((b-a)/RANGESIZE)<(nproc-1) + 1) {
+
+  if (((b - a) / RANGESIZE) < 2 * (nproc - 1))
+  {
     printf("More subranges needed");
     MPI_Finalize();
     return -1;
   }
 
-
   // run your computations here (including MPI communication)
   // now the master will distribute the data and slave processes will perform computations
-  if (myrank == 0) { //Master
-    unsigned long int* numbersToSend = (unsigned long int*)malloc(RANGESIZE * sizeof(unsigned long int)); //TODO: Might be problematic (BUFFER)
+  if (myrank == 0)
+  {                                                                                                        // Master
+    unsigned long int *numbersToSend = (unsigned long int *)malloc(RANGESIZE * sizeof(unsigned long int)); // TODO: Might be problematic (BUFFER)
     range[0] = a;
-    for (int i = 1; i < nproc; i++) {
-      range[1]=range[0]+RANGESIZE;
+    for (int i = 1; i < nproc; i++)
+    {
+      range[1] = range[0] + RANGESIZE;
 
-      for (int j=range[0]; j<range[1]; j++) {
-        numbersToSend[j-range[0]] = numbers[j];
+      for (int j = range[0]; j < range[1]; j++)
+      {
+        numbersToSend[j - range[0]] = numbers[j];
       }
 
-      rangeToSend = range[1]-range[0];
-      MPI_Send(numbersToSend,rangeToSend,MPI_UNSIGNED_LONG,i,DATA,MPI_COMM_WORLD);
-      sentcount++;
+      rangeToSend = range[1] - range[0];
+      MPI_Send(numbersToSend, rangeToSend, MPI_UNSIGNED_LONG, i, DATA, MPI_COMM_WORLD);
 
-      range[0]=range[1];
+      range[0] = range[1];
     }
 
-    do {
+    do
+    {
 
       // distribute remaining subranges to the processes which have completed their parts
 
-      MPI_Recv(&resulttemp,1,MPI_UNSIGNED_LONG,MPI_ANY_SOURCE,RESULT,MPI_COMM_WORLD,&status);
-      result+=resulttemp;
+      MPI_Recv(&resulttemp, 1, MPI_UNSIGNED_LONG, MPI_ANY_SOURCE, RESULT, MPI_COMM_WORLD, &status);
+      result += resulttemp;
 
-      range[1]=range[0]+RANGESIZE;
-      if (range[1]>b) range[1]=b;
+      range[1] = range[0] + RANGESIZE;
+      if (range[1] > b)
+        range[1] = b;
 
-      for (int j=range[0]; j<range[1]; j++) {
-        numbersToSend[j-range[0]] = numbers[j];
+      for (int j = range[0]; j < range[1]; j++)
+      {
+        numbersToSend[j - range[0]] = numbers[j];
       }
 
-      rangeToSend = range[1]-range[0];
-      MPI_Send(numbersToSend,rangeToSend,MPI_UNSIGNED_LONG,status.MPI_SOURCE,DATA,MPI_COMM_WORLD);
-      range[0]=range[1];
+      rangeToSend = range[1] - range[0];
+      MPI_Send(numbersToSend, rangeToSend, MPI_UNSIGNED_LONG, status.MPI_SOURCE, DATA, MPI_COMM_WORLD);
+      range[0] = range[1];
 
-    } while (range[1]<b);
+    } while (range[1] < b);
 
-    for (int i=0; i<nproc-1; i++) {
+    for (int i = 0; i < nproc - 1; i++)
+    {
 
-      MPI_Recv(&resulttemp,1,MPI_UNSIGNED_LONG,MPI_ANY_SOURCE,RESULT,MPI_COMM_WORLD,&status);
+      MPI_Recv(&resulttemp, 1, MPI_UNSIGNED_LONG, MPI_ANY_SOURCE, RESULT, MPI_COMM_WORLD, &status);
 
-      result+=resulttemp;
+      result += resulttemp;
     }
 
-    for (int i=1; i<nproc; i++) {
+    for (int i = 1; i < nproc; i++)
+    {
 
-      MPI_Send(NULL,0,MPI_UNSIGNED_LONG,i,FINISH,MPI_COMM_WORLD);
-
+      MPI_Send(NULL, 0, MPI_UNSIGNED_LONG, i, FINISH, MPI_COMM_WORLD);
     }
 
-    printf("\nMASTER: there are %ld primes\n",result);
+    printf("\nMASTER: there are %ld primes\n", result);
     free(numbersToSend);
-
-  } else { //SLAVE
+  }
+  else
+  { // SLAVE
     int count;
-    do {
+    do
+    {
 
-      MPI_Probe(0,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+      MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-      if (status.MPI_TAG==DATA) {
-        MPI_Get_count(&status, MPI_UNSIGNED_LONG, &count);         
-        unsigned long int* receivedNumbers = (unsigned long int*)malloc(count * sizeof(unsigned long int)); //TODO: Might be problematic (BUFFER)
+      if (status.MPI_TAG == DATA)
+      {
+        MPI_Get_count(&status, MPI_UNSIGNED_LONG, &count);
+        unsigned long int *receivedNumbers = (unsigned long int *)malloc(count * sizeof(unsigned long int)); // TODO: Might be problematic (BUFFER)
 
-        MPI_Recv(receivedNumbers,count,MPI_UNSIGNED_LONG,0,DATA,MPI_COMM_WORLD,&status);
-        
-        resulttemp=CheckHowManyPrimes(receivedNumbers, count);
+        MPI_Recv(receivedNumbers, count, MPI_UNSIGNED_LONG, 0, DATA, MPI_COMM_WORLD, &status);
 
-        
-        MPI_Send(&resulttemp,1,MPI_UNSIGNED_LONG,0,RESULT,MPI_COMM_WORLD);
+        resulttemp = CheckHowManyPrimes(receivedNumbers, count);
+
+        MPI_Send(&resulttemp, 1, MPI_UNSIGNED_LONG, 0, RESULT, MPI_COMM_WORLD);
         free(receivedNumbers);
       }
 
-    } while (status.MPI_TAG!=FINISH);
+    } while (status.MPI_TAG != FINISH);
+  }
 
-  } 
-
-
-  if (!myrank) {
+  if (!myrank)
+  {
     gettimeofday(&ins__tstop, NULL);
     ins__printtime(&ins__tstart, &ins__tstop, ins__args.marker);
   }
-  
+
   MPI_Finalize();
   return 0;
-
 }
