4761c72f88085d8cccecece1f2a1aaf4958cb629
markjeli <markandjel@gmail.com>
2024-05-24 08:21:27 +0200

    Cuda implementation of prime count check

--- cuda.cu
+++ cuda.cu
@@ -5,6 +5,34 @@
 #include <sys/time.h>
 #include "numgen.c"
 
+__device__
+unsigned long isPrime(unsigned long n)
+{
+  // Corner case
+  if (n <= 1)
+    return 0;
+
+  // Check from 2 to n-1
+  for (int i = 2; i <= sqrt(n); i++)
+    if (n % i == 0)
+      return 0;
+
+  return 1;
+}
+
+__global__
+void CheckHowManyPrimes(unsigned long numbers[], unsigned long *primes, long size)
+{
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  if (index < size)
+  {
+    if (isPrime(numbers[index]))
+    {
+      atomicAdd(primes, 1);
+    }
+  }
+}
+
 int main(int argc, char **argv)
 {
 
@@ -20,11 +48,28 @@ int main(int argc, char **argv)
   gettimeofday(&ins__tstart, NULL);
 
   // run your CUDA kernel(s) here
+  unsigned long *d_numbers;
+  unsigned long *d_primes;
+  unsigned long primes = 0;
 
-  for (long i = 0; i < inputArgument; i++)
-  {
-    printf("%ld\n", numbers[i]);
-  }
+  cudaMalloc(&d_numbers, inputArgument * sizeof(unsigned long));
+  cudaMalloc(&d_primes, sizeof(unsigned long));
+
+  cudaMemcpy(d_numbers, numbers, inputArgument * sizeof(unsigned long), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_primes, &primes, sizeof(unsigned long), cudaMemcpyHostToDevice);
+
+  int blockSize = 256;
+  int gridSize = (inputArgument + blockSize - 1) / blockSize;
+
+  CheckHowManyPrimes<<<gridSize, blockSize>>>(d_numbers, d_primes, inputArgument);
+
+  cudaMemcpy(&primes, d_primes, sizeof(unsigned long), cudaMemcpyDeviceToHost);
+  printf("Number of primes: %ld\n", primes);
+
+  cudaFree(d_numbers);
+  cudaFree(d_primes);
+
+  free(numbers);
 
   // synchronize/finalize your CUDA computations
 
